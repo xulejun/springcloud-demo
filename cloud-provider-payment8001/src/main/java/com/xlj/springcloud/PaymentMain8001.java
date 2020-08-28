e53ddb84c28cfd254796e1016ac3bc8392c36c10
xulejun <xulejun@mysteel.com>
2020-08-28 17:36:27 +0800

    主工程环境搭建

---
+++ cloud-provider-payment8001/src/main/java/com/xlj/springcloud/PaymentMain8001.java
@@ -0,0 +1,15 @@
+package com.xlj.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author XLJ
+ * @Date 2020/8/28
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
