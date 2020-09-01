55b1cab80382abe16cfbf9443bb7572cdaf77ae4
xulejun <xulejun@mysteel.com>
2020-09-01 19:55:26 +0800

    feat Eureka服务注册实现双集群

--- cloud-eureka-server7001/src/main/java/com/xlj/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/xlj/springcloud/EurekaMain7001.java
@@ -2,16 +2,16 @@ package com.xlj.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @Author XLJ
  * @Date 2020/9/1
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain80 {
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class,args);
+        SpringApplication.run(EurekaMain7001.class,args);
     }
 }
--- cloud-provider-payment8001/src/main/java/com/xlj/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/xlj/springcloud/PaymentMain8001.java
@@ -2,12 +2,14 @@ package com.xlj.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Author XLJ
  * @Date 2020/8/28
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class,args);
