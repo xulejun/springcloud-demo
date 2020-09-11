5a7f8712407965763381c4357d510c3a8673b62d
xulejun <xulejun@mysteel.com>
2020-09-11 15:58:02 +0800

    feat Hystrix服务提供者降级Fallback

--- cloud-provider-hystrix-payment8001/src/main/java/com/xlj/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/xlj/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.xlj.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -15,23 +17,29 @@ public class PaymentService {
      * @description: 成功访问
      * @author XLJ
      * @date 2020/9/11 9:46
-    */
-    public String getSuccess(Integer id){
-        return "线程池："+Thread.currentThread().getName()+"    is OK  ,id："+id;
+     */
+    public String getSuccess(Integer id) {
+        return "线程池：" + Thread.currentThread().getName() + "    is OK  ,id：" + id;
     }
 
     /**
      * @description: 超时访问
      * @author XLJ
      * @date 2020/9/11 9:47
-    */
-    public String getTimeout(Integer id){
-        int time = 3;
+     */
+    @HystrixCommand(fallbackMethod = "getTimeoutHandle", commandProperties =
+            {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")})
+    public String getTimeout(Integer id) {
+        int time = 5;
         try {
             TimeUnit.SECONDS.sleep(time);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        return "线程池："+Thread.currentThread().getName()+"is Timeout , id："+id+"，耗时："+time;
+        return "线程池：" + Thread.currentThread().getName() + "is Timeout , id：" + id + "，耗时：" + time;
+    }
+
+    public String getTimeoutHandle(Integer id) {
+        return "线程池：" + Thread.currentThread().getName() + "被调用 , getTimeout方法超时";
     }
 }
