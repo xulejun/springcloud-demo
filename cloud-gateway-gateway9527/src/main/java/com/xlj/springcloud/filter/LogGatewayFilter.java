c1ae64ed5730ca25c46abe4230198237fca10984
xulejun <xulejun@mysteel.com>
2020-09-15 17:07:49 +0800

    feat Gateway自定义过滤器配置

---
+++ cloud-gateway-gateway9527/src/main/java/com/xlj/springcloud/filter/LogGatewayFilter.java
@@ -0,0 +1,39 @@
+package com.xlj.springcloud.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
+
+/**
+ * 配置Gateway过滤器
+ * @author XLJ
+ * @date 2020/9/15
+ */
+@Component
+@Slf4j
+public class LogGatewayFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("*****come in LogGatewayFilter"+ new Date());
+        String username = exchange.getRequest().getQueryParams().getFirst("username");
+        if (StringUtils.isEmpty(username)){
+            log.info("***用户名为空，非法进入");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
