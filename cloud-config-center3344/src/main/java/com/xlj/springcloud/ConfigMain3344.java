e79002edf74d002ea0964bfd8c540bac642689c9
xulejun <xulejun@mysteel.com>
2020-09-15 19:05:24 +0800

    feat Config项目搭建、Git远程访问配置文件

---
+++ cloud-config-center3344/src/main/java/com/xlj/springcloud/ConfigMain3344.java
@@ -0,0 +1,18 @@
+package com.xlj.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author XLJ
+ * @date 2020/9/15
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigMain3344 {
+    public static void main(String[] args) {
+        // http://config3344:3344/test/config-test.yml 访问远程git配置文件
+        SpringApplication.run(ConfigMain3344.class, args);
+    }
+}
