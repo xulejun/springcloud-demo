97dedf9bdab1c542645f5b2de8883e51638622fc
xulejun <xulejun@mysteel.com>
2020-09-15 16:01:53 +0800

    feat Gateway断言扩充、时间地区显示

---
+++ cloud-gateway-gateway9527/src/main/java/com/xlj/springcloud/util/TimeZone.java
@@ -0,0 +1,15 @@
+package com.xlj.springcloud.util;
+
+import java.time.ZonedDateTime;
+
+/**
+ * @author XLJ
+ * @date 2020/9/15
+ */
+
+public class TimeZone {
+    public static void main(String[] args) {
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);
+    }
+}
