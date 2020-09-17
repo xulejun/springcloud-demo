95a0c16ea27798bd352058c09bec4519e86c86b7
xulejun <xulejun@mysteel.com>
2020-09-17 16:49:36 +0800

    feat Stream解决消息重复消费，持久化

--- cloud-stream-rabbitmq-consumer8803/src/main/java/springcloud/controller/MessageController.java
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/springcloud/controller/MessageController.java
@@ -1,4 +1,4 @@
-package com.xlj.springcloud.controller;
+package springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.stream.annotation.EnableBinding;
