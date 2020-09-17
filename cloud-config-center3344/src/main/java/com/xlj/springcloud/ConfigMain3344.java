6f98224a05dd0f7c096ec6b73632215087582cbd
xulejun <xulejun@mysteel.com>
2020-09-17 11:38:16 +0800

    style Config与Bus结合实现定点动态刷新

--- cloud-config-center3344/src/main/java/com/xlj/springcloud/ConfigMain3344.java
+++ cloud-config-center3344/src/main/java/com/xlj/springcloud/ConfigMain3344.java
@@ -12,7 +12,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class ConfigMain3344 {
     public static void main(String[] args) {
-        // http://config3344:3344/master/config-dev.yml 访问远程git配置文件
+        // http://config3344:3344/master/config-dev.yml    访问远程git配置文件
+        // http://localhost:3344/actuator/bus-refresh   Bus通知所有服务（要用POST请求，浏览器默认为GET）
+        // http://localhost:3344/actuator/bus-refresh/cloud-config-server:3355 只通知3355客户端（要用POST请求，浏览器默认为GET）
         SpringApplication.run(ConfigMain3344.class, args);
     }
 }
