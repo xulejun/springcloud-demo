bdca3fcd54c3e42cc86c603727dcc9d22e22653f
xulejun <xulejun@mysteel.com>
2020-09-30 15:10:30 +0800

    feat Sentinel热点限流，降级测试

--- cloudalibaba-sentinel-service8401/src/main/java/com/xlj/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/xlj/springcloud/controller/FlowLimitController.java
@@ -1,28 +1,66 @@
 package com.xlj.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author XLJ
  * @date 2020/9/29
  */
 @RestController
+@Slf4j
 public class FlowLimitController {
     @GetMapping(value = "/testA")
-    public String testA(){
+    public String testA() {
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         return "testA";
-
     }
 
     @GetMapping(value = "/testB")
-    public String testB(){
+    public String testB() {
         return "testB";
 
     }
+
+    @GetMapping("/testD")
+    public String testD() {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("testD 测试RT");
+        return "------testD";
+    }
+
+    @GetMapping("/testE")
+    public String testE() {
+        log.info("testE 测试异常比例");
+        int age = 10 / 0;
+        return "------testE";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2) {
+        //int age = 10/0;
+        return "------testHotKey";
+    }
+    //兜底方法
+    public String deal_testHotKey(String p1, String p2, BlockException exception) {
+        return "------deal_testHotKey,o(╥﹏╥)o";
+    }
+
+
 }
